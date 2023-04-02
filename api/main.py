9669138eddd14b4ccc4591702ea8a5d9f4c622e8
Jason Antwi-Appah <hey@jasonaa.me>
2023-04-02 00:56:02 -0500

    changes

--- api/main.py
+++ api/main.py
@@ -1,3 +1,4 @@
+import os
 import uuid
 
 from models import Persona, Conversation, User, TextCompletionBody
@@ -5,6 +6,8 @@ from .scripts import speech2text, parse
 import firebase_admin
 from firebase_admin import credentials, firestore
 import datetime
+from pydub import AudioSegment
+
 
 cred = credentials.Certificate("./api/secret.json")
 firebase_admin.initialize_app(cred)
@@ -24,14 +27,14 @@ prompt_base = "Your goal is to help your patient with their health needs. You ca
 
 personas: list[Persona] = [
     Persona(id=1, name="Dr. Cole", prompt="You are Dr. Cole, a fun, caring pediatric doctor.", professional_description="Dr. Cole is a fun, caring pediatric doctor. Use this persona for your younger patients."),
-    Persona(id=2, name="Dr. Smith", prompt="You are Dr. Smith, a fun, caring geriatric doctor. Be extra nice, as your patients are elderly and would love to have a conversation. Be sure to constantly reaffirm your love and care for your patient.", professional_description="Dr. Smith is a fun, caring geriatric doctor. Perfect for your older patients."),
+    Persona(id=2, name="Dr. Smith", prompt="You are Dr. Smith, a fun, caring geriatric doctor. Be extra nice, as your patients are elderly and would love to have a conversation. Verbally reaffirm your love and care for your patient to increase their comfort.", professional_description="Dr. Smith is a fun, caring geriatric doctor. Perfect for your older patients."),
 ]
 
 for persona in personas:
     persona.prompt = f"{persona.prompt} {prompt_base}"
 
 conversations: list[Conversation] = []
-users: list[User] = []
+users: list[User] = [User(id=0, name="Angela Thomas", health_history="", conversations=[], persona=personas[1])]
 
 
 app = FastAPI()
@@ -48,13 +51,33 @@ async def get_personas() -> list[Persona]:
     return personas
 
 
+
+@app.post("/conversation/new")
+async def new_conversation(user_id: int) -> Conversation:
+    conversation_id = len(conversations)
+    conversations.append(Conversation(id=conversation_id, user_id=user_id, messages=[]))
+    return conversations[conversation_id]
+
 @app.post("/conversation/{conversation_id}/audio")
 async def completion_audio(conversation_id: int, file: UploadFile) -> Conversation:
-    path = "../assets/" + str(uuid.uuid4()) + ".wav"
+    path = "../assets/" + str(uuid.uuid4()) + ""
     with open(path, "wb") as buffer:
         content = await file.read()
         buffer.write(content)
-    text = speech2text(path)
+
+    audio = AudioSegment.from_file(path)
+    new_path = path + ".wav"
+    audio.export(
+        path=new_path,
+        format=".wav",
+        bitrate="192k",
+        codec="pcm_mulaw",
+    )
+
+    os.remove(path)
+    text = speech2text(new_path)
+    os.remove(new_path)
+
     return completion_text(conversation_id, text)
 
 
@@ -88,8 +111,7 @@ def completion_text(conversation_id: int, message: str) -> Conversation:
     })
 
     if conversation_id not in conversations:
-        conversation_id = len(conversations)
-        conversations.append(Conversation(id=conversation_id, user_id=0, messages=[]))
+        raise Exception("Conversation not found")
         
     convo = conversations[conversation_id]
     user = users[convo.user_id]
@@ -102,17 +124,16 @@ def completion_text(conversation_id: int, message: str) -> Conversation:
             "content": m.text
         })
 
-    msgs.append({
-        "role": "user",
-        "content": message
-    })
-
     completion = openai.ChatCompletion.create(messages=[
         {
             "role": "system",
             "content": user.persona.prompt
         },
         *msgs,
+        {
+            "role": "user",
+            "content": message
+        }
     ], model="gpt-3.5-turbo")
     
     
